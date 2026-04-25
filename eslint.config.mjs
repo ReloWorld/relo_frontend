4c43915a50382639dcc5d461e0194c48f55aa279
godfr <gowusu018@gmail.com>
2026-04-25 15:34:48 +0200

    ci-cd: RELO-29 testing my workflow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,49 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+// import importPlugin from 'eslint-plugin-import';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    plugins: {
+      prettier: prettierPlugin,
+      // import: importPlugin,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'prettier/prettier': 'warn',
+
+      // ✅ Import correctness (light but useful)
+      'import/no-unresolved': 'error',
+      'import/no-duplicates': 'error',
+      'import/newline-after-import': 'warn',
+
+      // ✅ Clean import order (no headaches)
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+    },
+    settings: {
+      // Helps eslint-plugin-import resolve TS paths like "@/..."
+      'import/resolver': {
+        typescript: true,
+      },
+    },
+  },
+
+  prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
