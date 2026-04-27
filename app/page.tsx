b2e5625c3f9a7711d9a95a887fd00d1e77e1fda7
godfr <gowusu018@gmail.com>
2026-04-27 20:36:16 +0200

    test: RELO-111 main deployment

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
           priority
         />
         <h1>This is staging and we are testing workflows</h1>
+        <p>changes in main</p>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
